1c3e5dce4367da0d1e46b954a49821c544687b53
Jerry-Yenque <jerry.yenque@hotmail.com>
2023-08-28 18:21:25 -0500

    Actualiza .gitignore para excluir package-lock.json

--- functions/api.js
+++ functions/api.js
@@ -1,11 +1,19 @@
 import express, { Router } from 'express';
 import serverless from 'serverless-http';
+// const fs = require('fs');
 
 const api = express();
 
-const router = Router();
-router.get('/hello', (req, res) => res.send('Hello World!'));
+//  Setting the Template Engine
+// api.engine('pug', require('pug').__express)
+api.set('view engine' , 'pug');
 
-api.use('/api/', router);
+// Routing
+const router = Router();
+// router.get('/', (req, res) => res.send(fi));
+router.get('/', (req, res) => res.render('login'));
+router.get('/fhgf', (req, res) => res.render('login'));
+
+api.use('/', router);
 
 export const handler = serverless(api);
